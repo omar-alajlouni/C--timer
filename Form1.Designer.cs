eed1cc31ba7f30ee4f4f5f13ea9cbd5b48b3af49
omar-alajlouni <omar.alajlouni7@gmail.com>
2022-11-06 06:46:01 +0200

    all

---
+++ Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Time = new System.Windows.Forms.Timer(this.components);
+            this.lblClock = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.lblfoater = new System.Windows.Forms.Label();
+            this.lblLeft = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.lblMid = new System.Windows.Forms.Label();
+            this.timSplash = new System.Windows.Forms.Timer(this.components);
+            this.timProgress = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.ForeColor = System.Drawing.Color.Red;
+            this.lblCount.Location = new System.Drawing.Point(690, 368);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 32);
+            this.lblCount.TabIndex = 0;
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoading.ForeColor = System.Drawing.Color.Black;
+            this.lblLoading.Location = new System.Drawing.Point(116, 359);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(209, 44);
+            this.lblLoading.TabIndex = 1;
+            this.lblLoading.Text = "Loading ...";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.AccessibleRole = System.Windows.Forms.AccessibleRole.Caret;
+            this.progressBar1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.progressBar1.Location = new System.Drawing.Point(382, 359);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(271, 44);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Time
+            // 
+            this.Time.Enabled = true;
+            this.Time.Interval = 1000;
+            this.Time.Tick += new System.EventHandler(this.Time_Tick);
+            // 
+            // lblClock
+            // 
+            this.lblClock.AutoSize = true;
+            this.lblClock.Font = new System.Drawing.Font("Impact", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClock.ForeColor = System.Drawing.Color.Black;
+            this.lblClock.Location = new System.Drawing.Point(595, 56);
+            this.lblClock.Name = "lblClock";
+            this.lblClock.Size = new System.Drawing.Size(0, 46);
+            this.lblClock.TabIndex = 3;
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // lblfoater
+            // 
+            this.lblfoater.BackColor = System.Drawing.Color.White;
+            this.lblfoater.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfoater.ForeColor = System.Drawing.Color.Black;
+            this.lblfoater.Location = new System.Drawing.Point(2, 419);
+            this.lblfoater.Name = "lblfoater";
+            this.lblfoater.Size = new System.Drawing.Size(638, 32);
+            this.lblfoater.TabIndex = 6;
+            this.lblfoater.Text = "© CopyRight, All programs were reserved by laws in your country";
+            this.lblfoater.Click += new System.EventHandler(this.lblfoater_Click);
+            // 
+            // lblLeft
+            // 
+            this.lblLeft.Font = new System.Drawing.Font("Microsoft Uighur", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLeft.Location = new System.Drawing.Point(4, 47);
+            this.lblLeft.Name = "lblLeft";
+            this.lblLeft.Size = new System.Drawing.Size(33, 103);
+            this.lblLeft.TabIndex = 4;
+            this.lblLeft.Text = "•\r\n•\r\n•";
+            this.lblLeft.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblLeft.Click += new System.EventHandler(this.lblLeft_Click);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.label1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 45);
+            this.label1.TabIndex = 7;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.BackColor = System.Drawing.Color.MidnightBlue;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.LimeGreen;
+            this.lblWelcome.Location = new System.Drawing.Point(241, 6);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(319, 32);
+            this.lblWelcome.TabIndex = 8;
+            this.lblWelcome.Text = "Welcome in my program";
+            // 
+            // timer4
+            // 
+            this.timer4.Enabled = true;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // lblMid
+            // 
+            this.lblMid.AutoSize = true;
+            this.lblMid.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMid.Location = new System.Drawing.Point(383, 226);
+            this.lblMid.Name = "lblMid";
+            this.lblMid.Size = new System.Drawing.Size(39, 51);
+            this.lblMid.TabIndex = 9;
+            this.lblMid.Text = "*";
+            // 
+            // timSplash
+            // 
+            this.timSplash.Enabled = true;
+            this.timSplash.Interval = 25000;
+            this.timSplash.Tick += new System.EventHandler(this.timSplash_Tick);
+            // 
+            // timProgress
+            // 
+            this.timProgress.Enabled = true;
+            this.timProgress.Tick += new System.EventHandler(this.timProgress_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Timer.Properties.Resources.Presentation11;
+            this.pictureBox2.Location = new System.Drawing.Point(48, 154);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(702, 104);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Timer.Properties.Resources.giphy;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 336);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(77, 87);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblfoater);
+            this.Controls.Add(this.lblClock);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblMid);
+            this.Controls.Add(this.lblLeft);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Starting ...";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label lblLoading;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer Time;
+        private System.Windows.Forms.Label lblClock;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label lblfoater;
+        private System.Windows.Forms.Label lblLeft;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Label lblMid;
+        private System.Windows.Forms.Timer timSplash;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timProgress;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        int i;
+        bool show = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            i = 0;
+            lblWelcome.Left = -250;
+            lblfoater.Left = 700;
+            lblLeft.Top = 0; lblLeft.Left = 0;
+            lblMid.Top = 200; lblMid.Left = 300;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (i == 100)
+                timer1.Enabled = false;
+            else
+            {
+                i++; //i+2  i+5 .....
+                lblCount.Text = i.ToString() + "%"; //.ToString for convert to string 
+            }
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (show) //if (show == true) this equal if (show) so by defult while !show = false
+            {
+                lblLoading.Visible = show; //true
+                lblLoading.BackColor = Color.Red;
+                lblLoading.ForeColor = Color.White;
+                show = false;
+            }
+            else
+            {
+                lblLoading.Visible = show; //false
+                lblLoading.BackColor = Color.FromArgb(0,255,0);
+                show = true;
+            }
+            /*  lblLoading.Visible = show;    we can use this only
+                show = !show;                 without if statement  */
+            if (i == 100)
+            {
+                lblLoading.Visible = true;
+                lblLoading.Text = "Complete";
+            }
+            while (i == 100)
+            {
+                progressBar1.Step = i;
+                break;
+            }
+        }
+        private void progressBar1_Click(object sender, EventArgs e) {}
+        private void Time_Tick(object sender, EventArgs e)
+        {
+            DateTime time = new DateTime();
+            time = DateTime.Now;
+            lblClock.Text = time.ToLongTimeString().ToString();
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            bool inside = ClientRectangle.IntersectsWith(lblLeft.Bounds);
+            if (lblWelcome.Left > 700 && lblfoater.Left < -600)
+            {
+                lblWelcome.Left = -250;
+                lblfoater.Left = 600;
+            }
+            else
+            {
+                lblWelcome.Left += 1;
+                lblfoater.Left -= 1;
+            }
+            if(!inside)
+            {
+                lblLeft.Top = 0;
+            }
+            lblLeft.Top += 1;
+           /* if (lblLeft.Left > 800&& lblLeft.Top>550)
+            {
+                lblLeft.Left = -50;
+                lblLeft.Top = -50;
+            }
+            else
+            {
+                lblLeft.Left += 1;
+            }*/
+        }
+        private void lblfoater_Click(object sender, EventArgs e) {}
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (lblMid.Top > 350 && lblMid.Left > 350) 
+            {
+                lblMid.Top = 0;
+                lblMid.Left = 0;
+            }
+            else
+            {
+                lblMid.Top += 3;
+                lblMid.Left += 3;
+            }
+        }
+        private void timSplash_Tick(object sender, EventArgs e)
+        {
+            this.Hide();
+            splash ob = new splash();
+            ob.Show();
+            timSplash.Enabled = false;
+        }
+        private void timProgress_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(1);
+        }
+        private void lblLeft_Click(object sender, EventArgs e) {}
+    }
+}
